49e120f0bf12220b42c525a195197fec38746179
GaugeZich <guilleop.vale@gmail.com>
2025-08-30 18:36:54 -0300

    Avance de funcionalidad 1. Falta agregar los inputs. Mejora del formulario y cambio de nombre CSS

---
+++ main.js
@@ -0,0 +1,20 @@
+/*
+    
+*/
+document.querySelectorAll('input[name="radioVehiculo"]').forEach(input => {
+    input.addEventListener("change", () => {
+        document.getElementById("btn_Vehiculo").disabled = false;
+    });
+});
+
+document.querySelectorAll('input[name="radioColectivo"]').forEach(input => {
+    input.addEventListener("change", () => {
+        document.getElementById("btn_Colectivo").disabled = false;
+    });
+});
+
+document.querySelectorAll('input[name="radioAlimentos"]').forEach(input => {
+    input.addEventListener("change", () => {
+        document.getElementById("btn_Alimentacion").disabled = false;
+    });
+});
