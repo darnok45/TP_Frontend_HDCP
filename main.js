cebc07531cf693c295e28f8e910c7554b1d5721e
GaugeZich <guilleop.vale@gmail.com>
2025-08-30 18:36:58 -0300

    Avance de funcionalidad 1. Falta agregar los inputs. Mejora del formulario y cambio de nombre CSS

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /*
-    
+    Funciones para habilitar los botones cuando se presiona un Input radio
 */
 document.querySelectorAll('input[name="radioVehiculo"]').forEach(input => {
     input.addEventListener("change", () => {
