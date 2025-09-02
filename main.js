d3c3934a45e73a92cad960d886f6119e26b25b68
darnok45 <parragenaro429@gmail.com>
2025-09-02 19:43:11 -0300

    cambios varios

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-themeToggle.addEventListener('change', () => {
-    if (themeToggle.checked) {
-        document.body.classList.add('dark');
-    } else {
-        document.body.classList.remove('dark');
-    }
-});
 
 
 
